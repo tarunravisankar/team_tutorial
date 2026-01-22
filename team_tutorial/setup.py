c3399d27fb670cae8c0a5023a5979d043956d103
William Patteson <hudson1@cs.utexas.edu>
2026-01-21 21:08:50 -0600

    node b entry point again

--- team_tutorial/setup.py
+++ team_tutorial/setup.py
@@ -1,12 +1,6 @@
 entry_points={
         'console_scripts': [
             'node_a = team_tutorial.node_a:main',
+            'node_b = team_tutorial.node_b:main',
         ],
     },
-
-entry_points={
-    'console_scripts': [
-        'node_a = team_tutorial.node_a:main',
-        'node_b = team_tutorial.node_b:main',
-    ],
-},
